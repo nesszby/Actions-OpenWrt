127c00f9a73de4756cf580a57532bbd6d0bc8ea7
Django9264 <82198758+Django9264@users.noreply.github.com>
2021-10-05 19:10:09 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,14 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+# Modify default theme
+sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# Modify default password
+#sed -i 's/root:.*/root:$1$c.HkM.0F$wmPKu7xbctli1iHA1Crrg.:18862:0:99999:7:::/g' package/base-files/files/etc/shadow
+# Modify other ipk
+rm -rf package/lean/luci-theme-argon
+git clone https://github.com/Django9264/luci-app-tencentddns package/luci-app-tencentddns
+git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+git clone https://github.com/Django9264/luci-theme-argon package/luci-theme-argon
+
